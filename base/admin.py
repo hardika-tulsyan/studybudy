a566456f7e84ea496a97990280a9255b114473c7
Hardika Tulsyan <hardika.tulsyan@gmail.com>
2022-09-08 13:53:43 -0400

    First commit

---
+++ base/admin.py
@@ -0,0 +1,12 @@
+import imp
+from django.contrib import admin
+from .models import User, Room, Topic, Message
+# Register your models here.
+
+
+
+
+admin.site.register(User)
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
